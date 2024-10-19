eb52b62c960854e89a2feca764bc93a916dfface
Hiệu burando Tô <hieutotoka@gmail.com>
2024-10-19 10:29:05 +0700

    FInal

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EditForm from '@/components/EditForm.vue'
+import AddForm from '@/views/AddForm.vue'
+import DetailForm from '@/views/DetailForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/create',
+      name: 'add',
+      component: AddForm
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: DetailForm
+    },
+    {
+      path: '/edit/:id',
+      name: 'edit',
+      component: EditForm
     }
   ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,56 @@
+<template>
+  <main>
+      <RouterLink to="/create" class="btn btn-primary">
+          Create Nhân Viên
+      </RouterLink>
+      <table class="table">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>name</th>
+              <th>position</th>
+              <th>departmen</th>
+              <th>email:</th>
+              <th>phone</th>
+            </tr>
+          </thead>
+          <tbody>
+              <tr v-for="nhanVien in nhanviens" :key="nhanVien.id">
+                <td>{{ nhanVien.id }}</td>
+                <td>{{ nhanVien.name }}</td>
+                <td>{{ nhanVien.position }}</td>
+                <td>{{ nhanVien.departmen }}</td>
+                <td>{{ nhanVien.email }}</td>
+                <td>{{ nhanVien.phone }}</td>
+                <td>
+                  <RouterLink :to="{name: 'detail', params: {id: nhanVien.id}}" class="btn btn-info">View</RouterLink>
+                  <RouterLink :to="{name: 'edit', params: {id: nhanVien.id}}" class="btn btn-info">Edit</RouterLink>
+                  <button class="btn btn-danger" @click="handleDelete(nhanVien.id)">Delete</button>
+                </td>
+              </tr>
+          </tbody>
+      </table>
+  </main>
+</template>
+
 <script setup>
+import {ref, onMounted} from 'vue'
+import instanceAxios  from '@/ultis/configAxios';
+
+const nhanviens = async ()=> {
+  const response = await instanceAxios.get('/nhanviens')
+  nhanviens.value = response.data
+}
+
+const handleDelete = async (id) => {
+  const isConfirm = confirm(" bạn muốn xóa ko")
+  if(!isConfirm) return
+  await instanceAxios.delete(`/nhanviens/${id}`)
+  fetchnhanviens
+}
+
 </script>
 
-<template>
-  <main class="text-center">This is Home Page</main>
-</template>
+<style lang="scss" scoped>
+
+</style>
